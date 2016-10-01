ec791057589044970c19629313e767cecbc509f2
Athletesrun <benwingerter01@gmail.com>
2016-10-01 16:12:55 -0500

    Updated ben.json

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-  $.getJSON("https://raw.githubusercontent.com/GETHIP-Classroom/2017-hw2-Athletesrun/master/ben.json", function(data){
-    console.log(data);
-    $("#profile-name").text(data.firstName + " " + data.lastName);
-    $("#age").text(data.age);
-    $("#profile-picture").attr("src", data.picture);
-    $.each(data.nickNames, function(key, value){
-      $("#nicknames").append("<li>" + value.nick + "</li>");
+    $.getJSON("https://raw.githubusercontent.com/GETHIP-Classroom/2017-hw2-Athletesrun/master/ben.json", function(data){
+        console.log(data);
+        $("#profile-name").text(data.firstName + " " + data.lastName);
+        $("#age").text(data.age);
+        $("#profile-picture").attr("src", data.picture);
+        $.each(data.activities.sports, function(key, value){
+            console.log(value);
+            $("#sports").append("<li>" + value + "</li>");
+        })
+        $.each(data.activities.technology, function(key, value) {
+            $('#technology').append("<li>" + value + "</li>")
+        })
     });
-  });
 });
